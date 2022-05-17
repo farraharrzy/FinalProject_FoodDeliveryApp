3a643ac14e4749b299fe97170e6651383ad0a411
Farrah Arrazy <farraharrazyrazy@gmail.com>
2022-05-17 15:29:25 +0700

    add methods on FoodService

--- FoodService/GraphQL/Mutation.cs
+++ FoodService/GraphQL/Mutation.cs
@@ -1,6 +1,59 @@
-﻿namespace FoodService.GraphQL
+﻿using FoodService.Models;
+using HotChocolate.AspNetCore.Authorization;
+
+namespace FoodService.GraphQL
 {
     public class Mutation
     {
+        [Authorize(Roles = new[] { "MANAGER" })]
+        public async Task<Food> AddFoodAsync(
+            FoodInput input,
+            [Service] FoodDeliveryAppContext context)
+        {
+
+            // EF
+            var food = new Food
+            {
+                Name = input.Name,
+                Stock = input.Stock,
+                Price = input.Price,
+                Created = DateTime.Now
+            };
+
+            var ret = context.Foods.Add(food);
+            await context.SaveChangesAsync();
+
+            return ret.Entity;
+        }
+        public async Task<Food> UpdateFoodAsync(
+            FoodInput input,
+            [Service] FoodDeliveryAppContext context)
+        {
+            var food = context.Foods.Where(o => o.Id == input.Id).FirstOrDefault();
+            if (food != null)
+            {
+                food.Name = input.Name;
+                food.Stock = input.Stock;
+                food.Price = input.Price;
+
+                context.Foods.Update(food);
+                await context.SaveChangesAsync();
+            }
+
+            return await Task.FromResult(food);
+        }
+        public async Task<Food> DeleteFoodByIdAsync(
+            int id,
+            [Service] FoodDeliveryAppContext context)
+        {
+            var food = context.Foods.Where(o => o.Id == id).FirstOrDefault();
+            if (food != null)
+            {
+                context.Foods.Remove(food);
+                await context.SaveChangesAsync();
+            }
+
+            return await Task.FromResult(food);
+        }
     }
 }
--- FoodService/GraphQL/Query.cs
+++ FoodService/GraphQL/Query.cs
@@ -1,6 +1,12 @@
-﻿namespace FoodService.GraphQL
+﻿using FoodService.Models;
+using HotChocolate.AspNetCore.Authorization;
+
+namespace FoodService.GraphQL
 {
     public class Query
     {
+        [Authorize(Roles = new[] { "BUYER" })]
+        public IQueryable<Food> GetFoods([Service] FoodDeliveryAppContext context) =>
+            context.Foods;
     }
 }
